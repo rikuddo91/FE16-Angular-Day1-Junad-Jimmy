a14d1fbeb383ff76176ee3545a15ebcfbad9013f
Jimmy CHAVES <jimmybakerchaves@gmail.com>
2022-10-10 11:36:09 +0200

    create app

---
+++ TouristAgency/src/app/hero/hero.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.css']
+})
+export class HeroComponent implements OnInit {
+  title : string = "Winter is coming !";
+  paragraph : string = "Jingle bells ! Jingle bells! Jingle all the way! It is time to book your winter holiday!";
+  btn : string = "Learn more";
+  btnUrl : string = "";
+  why:string =""
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
